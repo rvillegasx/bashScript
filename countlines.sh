5486efc6681e287e0b18057d8997aa3e97c184dd
Roberto Villegas <rvillegas@gmail.com>
2023-06-07 19:42:32 -0600

    Adjusting to accept only one value

--- countlines.sh
+++ countlines.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-usage() { echo "Usage: $0 [-o <string>] [-m <1-12>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-o <string>] or [-m <1-12>]" 1>&2; exit 1; }
 
-while getopts ":o:m:" o; do
-    case "${o}" in
+while getopts ":o:m:" arg; do
+    #echo "arg = ${arg}"
+    #echo "optind = ${OPTIND}"
+    case ${arg} in
         o)
             ow=${OPTARG}
             ;;
         m)
-            m=${OPTARG}
-            ((m >= 45 || m <= 90)) || usage
+            mv=${OPTARG}
+            ((mv >= 1 && mv <= 12)) || usage
             ;;
         *)
             usage
             ;;
     esac
 done
-shift $((OPTIND-1))
-
-if [ -z "${o}" ] || [ -z "${m}" ]; then
+#shift $((OPTIND-1))
+# echo "optind = ${OPTIND}"
+# echo "$#"
+if [ $# -gt 2 ]; then
     usage
 fi
-
+echo "optind = ${OPTIND}"
 echo "o = ${ow}"
-echo "m = ${m}"
+echo "m = ${mv}"
