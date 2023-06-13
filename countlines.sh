9df72e897e0b7e145a09b10ee7cd2375e5f61df2
Roberto Villegas <rvillegas@gmail.com>
2023-06-13 17:23:23 -0600

    added month option

--- countlines.sh
+++ countlines.sh
@@ -2,40 +2,44 @@
 usage() { echo "Usage: $0 [-o <string>] or [-m <1-12>]" 1>&2; exit 1; }
 
 while getopts ":o:m:" arg; do
-    #echo "arg = ${arg}"
-    #echo "optind = ${OPTIND}"
     case ${arg} in
         o)
             ow=${OPTARG}
-            # for i in $(find . \( ! -regex '.*/\..*' \) -type f -maxdepth 1 -user ${ow})
-            # do
-            #     b=${i:2}
-            #     echo "$b"
-            # done
-            find . \( ! -regex '.*/\..*' \) -type f -maxdepth 1 -user ${ow} -print0 | while read -d $'\0' file
-            do
-                b=${file:2}
-                echo "$b"
-            done
             ;;
         m)
             mv=${OPTARG}
             ((mv >= 1 && mv <= 12)) || usage
-            # for i in ./*
             ;;
         *)
             usage
             ;;
     esac
 done
-#shift $((OPTIND-1))
-# echo "optind = ${OPTIND}"
-# echo "$#"
 if [ $# -gt 2 ]; then
     usage
 fi
-echo "optind = ${OPTIND}"
-echo "o = ${ow}"
-echo "m = ${mv}"
-
-# find . \( ! -regex '.*/\..*' \) -type f -maxdepth 1 -user rvillegas
+if [ -n "${ow}" ];
+then
+    find . \( ! -regex '.*/\..*' \) -type f -maxdepth 1 -user ${ow} -print0 | while read -d $'\0' file
+    do
+        f=${file:2}
+        l=$(wc $f | awk '{print $1}')
+        lc=$(($l + 1))
+        echo "File: $f, Lines: $lc $f"
+    done
+else
+    if [ -n "${mv}" ];
+    then
+        monthnames=(ZERO Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
+        month=${monthnames[${mv}]}
+        ls -l | grep -e $month | awk '{print $9}' | while read -r file
+        do
+            f=${file}
+            l=$(wc $f | awk '{print $1}')
+            lc=$(($l + 1))
+            echo "File: $f, Lines: $lc $f"
+        done
+    else
+        usage
+    fi
+fi
